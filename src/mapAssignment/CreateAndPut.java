6db30a6d92df76fe49b85f0e504af20cac5c7995
Arsen Keneshbekov <arsengithub1@gmail.com>
2026-02-15 02:14:02 +0600

    init commit

---
+++ src/mapAssignment/CreateAndPut.java
@@ -0,0 +1,11 @@
+package mapAssignment;
+import java.util.*;
+
+public class CreateAndPut {
+    static void main() {
+        System.out.println("Hello world");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a);
+    }
+}
---
+++ src/mapAssignment/NewTask.java
@@ -0,0 +1,7 @@
+package mapAssignment;
+
+public class NewTask {
+    static void main() {
+
+    }
+}
